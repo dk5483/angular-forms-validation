44757b0b11a6621898c404ce4260c9e77ff70cf7
Dhruw Kumar <dk5483159.star@gmail.com>
2020-10-30 00:03:48 +0530

    initial commit for template-driven-form-validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { PasswordPatternDirective } from './directives/password-pattern.directive';
+import { MatchPasswordDirective } from './directives/match-password.directive';
+import { ValidateUserNameDirective } from './directives/validate-user-name.directive';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordPatternDirective,
+    MatchPasswordDirective,
+    ValidateUserNameDirective,
+    TemplateDrivenFormComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: TemplateDrivenFormComponent },
+      { path: 'template-form', component: TemplateDrivenFormComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
